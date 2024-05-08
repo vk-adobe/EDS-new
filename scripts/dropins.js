c7b8b28a8f49865e478e912a0cc8a9fdeec8820f
vk-adobe <86674341+vk-adobe@users.noreply.github.com>
2024-05-08 13:26:09 -0400

    Initialize update-dropins

---
+++ scripts/dropins.js
@@ -0,0 +1,37 @@
+/* eslint-disable import/no-unresolved */
+// Drop-in Tools
+import { events } from '@dropins/elsie/event-bus.js';
+import { setEndpoint } from '@dropins/elsie/fetch-graphql.js';
+import { initializers } from '@dropins/elsie/initializer.js';
+
+// Drop-ins
+import * as cart from '@dropins/storefront-cart/api.js';
+
+// Libs
+import { getConfigValue } from './configs.js';
+
+export default async function initializeDropins() {
+  // Set Fetch Endpoint (Global)
+  setEndpoint(await getConfigValue('commerce-core-endpoint'));
+
+  // Initializers (Global)
+  initializers.register(cart.initialize, {});
+
+  // Cache cartId in session storage
+  events.on('cart/data', (data) => {
+    if (data?.id) {
+      sessionStorage.setItem('DROPINS_CART_ID', data.id);
+    } else {
+      sessionStorage.removeItem('DROPINS_CART_ID');
+    }
+  });
+
+  // Mount all registered drop-ins
+  if (document.readyState === 'complete') {
+    initializers.mount();
+  } else {
+    window.addEventListener('load', initializers.mount);
+  }
+
+  events.enableLogger(true);
+}
